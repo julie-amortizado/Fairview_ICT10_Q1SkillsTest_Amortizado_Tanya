c25fc75a183ff30150a56d081924d1118e492bea
julie <amortizado.ta@obmontessori.edu.ph>
2026-09-11 14:28:32 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from pyscript import display, document
+
+PRICES = {
+    "c1": 130.00,
+    "c2": 150.00,
+    "c3": 155.00,
+    "c4": 140.00,
+    "c5": 165.00,
+}
+
+TAX_RATE = 0.12
+
+def create_order(e):
+    subtotal = 0.0
+
+    for item_id, price in PRICES.items(): 
+        if document.getElementById(item_id).checked: 
+            subtotal += price
+
+    tax = subtotal * TAX_RATE 
+    total = subtotal + tax
+
+
+    receipt_div = document.getElementById("receipt")
+    receipt_div.innerHTML = "<h2>==== Receipt ====</h2>"
+
+    display(f"Subtotal: ₱{subtotal:.2f}", target="receipt")
+    display(f"Tax (12%): ₱{tax:.2f}", target="receipt") 
+    display(f"Total: ₱{total:.2f}", target="receipt")
